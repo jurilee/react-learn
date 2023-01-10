04855f026c1d9ffb7b02dbf3de6541b4249f768e
jurilee <jurilee@hivelab.co.kr>
2023-01-10 10:12:41 +0900

    add config

--- index.js
+++ index.js
@@ -3,15 +3,29 @@ const app = express();
 const port = 5000;
 const password = encodeURIComponent("jllee1295@@");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const { User } = require('./Users');
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 mongoose.set("strictQuery", false);
-mongoose
-  .connect(
-    `mongodb+srv://jurilee:${password}@react-learn.e6jf7ya.mongodb.net/?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
-  .then(() => console.log("mongoDB connected"))
-  .catch((err) => console.log("err.."));
+mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("mongoDB connected"))
+    .catch((err) => console.log("err.."));
 
 app.get("/", function (req, res) { res.send("hello world"); });
+
+app.post('/register', (req, res) => {
+    //회원가입 할 때 필요한 정보들을 클라이언트에서 가져온 뒤 DB에 적재
+    const user = new User(req.body);
+    user.save((err, userInfor) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        })
+    });
+})
+
 app.listen(port, () => console.log(`express app listening on ${port} servers on!`))
