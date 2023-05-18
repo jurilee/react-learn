2f5a3cc393dae80ebeb1bb89d0aaff742e922b22
jurilee <jurilee@hivelab.co.kr>
2023-05-18 21:16:31 +0900

    err

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,21 +1,54 @@
-import React from "react";
+import { Axios } from "axios";
+import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+import {loginUser} from '../../../_actions/user_actions'
 
-function loginPage() {
+function LoginPage(props) {
+    const [Email, setEmail] = useState("");
+    const [Password, setPassword] = useState("");
+    const onEmailHandler = (e) => {
+      setEmail(e.currentTarget.value);
+    };
+    const onPasswordHandler = (e) => {
+      setPassword(e.currentTarget.value);
+    };
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        setPassword(e.currentTarget.value);
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body)
+            .then(res => {
+                if (res.payLoad.loginSuccess) {
+                props.history.push('/')
+                } else {
+                    alert('error')
+            }
+        }))
+
+    };
+
     return (
         <div style={{
             display: 'flex', justifyContent: 'center', alignItems: 'center',
             width: '100%', height:'100vh'
         }}>
-        <form style={{display:'flex', flexDirection:'column'}}>
-            <label>Email</label>
-            <input type="email" value onChange></input>
-            <label>Password</label>
-            <input type="password" value onChange></input>
-            <br />
-            <button>Login</button>
-        </form>
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+            onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler}></input>
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler}></input>
+                <br />
+                <button>Login</button>
+            </form>
         </div>
     );
 }
 
-export default loginPage;
+export default LoginPage;
